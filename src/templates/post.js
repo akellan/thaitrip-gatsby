26b5feb5c9ef0602070d2b2dec01d326ced6577f
akellan <akella.n@gmail.com>
2019-02-18 23:19:39 +0200

    feature: create blogs list

---
+++ src/templates/post.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "../components/layout";
+import { graphql } from "gatsby";
+
+export default function BlogPost(props) {
+  const post = props.data.markdownRemark;
+  const { title, date } = post.frontmatter;
+  return (
+    <Layout>
+      <div>
+        <h1>{title}</h1>
+        <h2>{date}</h2>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  );
+}
+
+export const query = graphql`
+  query PostQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date(formatString: "D MMMM YYYY", locale: "ru-UA")
+      }
+    }
+  }
+`;

51979be0fec3e5dfba26b5696c8a36c61218aa10
akellan <akella.n@gmail.com>
2019-02-20 21:29:38 +0200

    fix global styles

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -4,17 +4,27 @@ import { Global, css } from "@emotion/core";
 export const GlobalStyles = () => (
   <Global
     styles={css`
+      * {
+        margin: 0;
+        padding: 0;
+      }
+
+      *,
+      *::before,
+      *::after {
+        box-sizing: inherit;
+      }
+
       html {
-        font-family: "Laila", sans-serif;
         box-sizing: content-box;
+      }
+
+      body {
+        font-family: "Laila", sans-serif;
       }
 
       * {
         box-sizing: inherit;
-      }
-
-      body {
-        margin: 0;
       }
 
       @import url("https://fonts.googleapis.com/css?family=Laila:500");

ed087e05deb87073fd823d5268002e54163d2e55
akellan <akella.n@gmail.com>
2019-03-18 23:52:04 +0200

    chore: load images from folders

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Layout } from "../components";
 import { graphql } from "gatsby";
+import Img from "gatsby-image";
 
 export default function BlogPost(props) {
   const post = props.data.markdownRemark;
+  const images = props.data.allFile.edges;
   const { title, date } = post.frontmatter;
   return (
     <Layout>
@@ -11,6 +13,11 @@ export default function BlogPost(props) {
         <h1>{title}</h1>
         <h2>{date}</h2>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
+        <div>
+          {images.map(({ node }, index) => (
+            <Img key={index} fixed={node.childImageSharp.fixed} />
+          ))}
+        </div>
       </div>
     </Layout>
   );
@@ -23,6 +30,23 @@ export const query = graphql`
       frontmatter {
         title
         date
+      }
+    }
+
+    allFile(
+      filter: {
+        extension: { regex: "/(jpg|jpeg)/i" }
+        relativeDirectory: { eq: $slug }
+      }
+    ) {
+      edges {
+        node {
+          childImageSharp {
+            fixed {
+              ...GatsbyImageSharpFixed
+            }
+          }
+        }
       }
     }
   }

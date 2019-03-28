cc5d31fcffebebb24e48e09bef3a4273ca68c305
akellan <akella.n@gmail.com>
2019-03-28 23:47:47 +0200

    chore: rename-folders

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { Layout } from "../components";
-import SEO from "../components/seo";
+import SEO from "../components/Seo";
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="404: Not found" description="Blog" />
     <h1>NOT FOUND</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>

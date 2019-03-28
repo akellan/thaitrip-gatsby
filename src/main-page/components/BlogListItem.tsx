ad4aa4f453d94b4401628805e09c73710887588e
akellan <akella.n@gmail.com>
2019-03-28 21:20:20 +0200

    fix styling for blog list item

--- src/main-page/components/BlogListItem.tsx
+++ src/main-page/components/BlogListItem.tsx
@@ -1,41 +1,47 @@
 import * as React from "react";
 import Img from "gatsby-image";
 import { HalfStyle } from "../../components/HalfStyle";
-import { Typography, Grid, withStyles } from "@material-ui/core";
+import {
+  Typography,
+  Grid,
+  withStyles,
+  createStyles,
+  WithStyles
+} from "@material-ui/core";
 import { AppLink } from "../../components/AppLink";
-import { ThemeOptions } from "@material-ui/core/styles/createMuiTheme";
+import { Theme } from "@material-ui/core/styles/createMuiTheme";
 
-interface BlogListItemProps {
+const styles = (theme: Theme) =>
+  createStyles({
+    articleLink: {
+      transition: "color 0.3s",
+      "&:hover": {
+        color: theme.palette.text.secondary
+      },
+
+      "&:hover > div": {
+        "box-shadow": "0px 5px 10px 1px rgba(0, 0, 0, 0.2)"
+      }
+    },
+    imageShadow: {
+      transition: "all 0.3s",
+      margin: "0.5rem 0"
+    },
+    articleTitleImage: {
+      border: "5px solid #fff",
+      "box-shadow": "0px 0px 0px 1px rgba(0, 0, 0, 0.2)",
+      margin: "0"
+    }
+  });
+
+interface BlogListItemProps extends WithStyles<typeof styles> {
   title: string;
   date: string;
   slug: string;
   image: any;
   excerpt: string;
-  classes: any;
   images: string;
 }
-
-const style = (theme: ThemeOptions) => ({
-  articleLink: {
-    transition: "color 0.3s",
-    "&:hover": {
-      color: theme.palette.text.secondary
-    },
-
-    "&:hover > div": {
-      "box-shadow": "0px 5px 10px 1px rgba(0, 0, 0, 0.2)"
-    }
-  },
-  imageShadow: {
-    transition: "all 0.3s",
-    margin: "0.5rem 0"
-  },
-  articleTitleImage: {
-    border: "5px solid #fff",
-    "box-shadow": "0px 0px 0px 1px rgba(0, 0, 0, 0.2)",
-    margin: "0"
-  }
-});
 
 const BlogListItemBase = ({
   title,
@@ -43,11 +49,10 @@ const BlogListItemBase = ({
   slug,
   image,
   excerpt,
-  classes,
-  images
+  classes
 }: BlogListItemProps) => {
   return (
-    <Grid item={true} xs={11} md={8} lg={6} container={true} direction="column">
+    <Grid item={true} sm={11} md={8} lg={6} container={true} direction="column">
       <AppLink
         underline="none"
         variant="h3"
@@ -76,4 +81,4 @@ const BlogListItemBase = ({
   );
 };
 
-export const BlogListItem = withStyles(style)(BlogListItemBase);
+export const BlogListItem = withStyles(styles)(BlogListItemBase);

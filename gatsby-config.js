20ed34522db7e71e8fd067efc4f72f5403c9f756
akellan <akella.n@gmail.com>
2019-03-29 12:46:57 +0200

    enable pwa manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,45 +1,45 @@
 module.exports = {
-  siteMetadata: {
-    title: `Thai Trip`,
-    description: `Thai Trip Blog`,
-    author: `Mykola`
-  },
-  plugins: [
-    `gatsby-plugin-webpack-size`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-catch-links`,
-    {
-      resolve: `gatsby-plugin-typescript`,
-      options: {
-        isTSX: true,
-        jsxPragma: `React`,
-        allExtensions: true
-      }
+    siteMetadata: {
+        title: `Thai Trip`,
+        description: `Thai Trip Blog`,
+        author: `Mykola`
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/src/posts-data`
-      }
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `gatsby-starter-default`,
-    //     short_name: `starter`,
-    //     start_url: `/`,
-    //     background_color: `#663399`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
-    //   }
-    // }
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.app/offline
-    // 'gatsby-plugin-offline',
-  ]
+    plugins: [
+        `gatsby-plugin-webpack-size`,
+        `gatsby-transformer-remark`,
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-catch-links`,
+        {
+            resolve: `gatsby-plugin-typescript`,
+            options: {
+                isTSX: true,
+                jsxPragma: `React`,
+                allExtensions: true
+            }
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `posts`,
+                path: `${__dirname}/src/posts-data`
+            }
+        },
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+        {
+            resolve: `gatsby-plugin-manifest`,
+            options: {
+                name: `Thai Trip`,
+                short_name: `Thai Trip`,
+                start_url: `/`,
+                background_color: `#fff`,
+                theme_color: `#1ac308`,
+                display: `minimal-ui`,
+                icon: `src/icons/palm-tree-icon.png`
+            }
+        }
+        // this (optional) plugin enables Progressive Web App + Offline functionality
+        // To learn more, visit: https://gatsby.app/offline
+        // 'gatsby-plugin-offline',
+    ]
 };
